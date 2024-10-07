8add5f52283749ed71674146c88e8231de478260
bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2024-10-07 05:48:24 +0000

    blog section: done

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import MobileHero from "@/components/mobileHero";
 import AboutMe from "@/components/about";
 import WhatIDo from "@/components/whatIDo";
 import MyWork from "@/components/myWork";
+import Insights from "@/components/insights";
 
 export default function Home() {
   return (
@@ -17,6 +18,9 @@ export default function Home() {
       </section>
       <section className="md:hidden bg-white mt-1">
         <MyWork />        
+      </section>
+      <section className="md:hidden bg-white mt-1">
+        <Insights />        
       </section>
     </>
   );
---
+++ components/insights.jsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { Card, CardContent } from "@/components/ui/card"; // Assuming you have these components set up correctly
+import Balancer from "react-wrap-balancer";
+import Image from "next/image";
+
+export default function Insights() {
+  const cardsContent = [
+    {
+      title: "The Future of Social Entrepreneurship: Trends and Opportunities for Young Innovators",
+      date: "Aug 2024",
+      description:
+        "Discussing emerging trends in social entrepreneurship and how young leaders can capitalize on them",
+      image: "/images/photo_5947026960783886263_y.jpg", // Correct the image path as needed
+    },
+    {
+      title: "Building a Sustainable Startup: Lessons from Leading Reecoplast",
+      date: "Sept 2024",
+      description:
+        "Sharing experiences and challenges in founding and growing a green business focused on sustainability",
+      image: "/images/tennis.jpg", // Correct the image path as needed
+    },
+    {
+      title: "Bridging the STEM gap in rural areas: Strategies for enhancing access to science and technology education",
+      date: "Sept 2024",
+      description:
+        "Offering strategies to improve STEM education in undeserved communities",
+      image: "/images/F_8kgOjbgAAKCDv.jpeg", // Correct the image path as needed
+    },
+  ];
+
+  return (
+    <div className="flex flex-col justify-center items-center text-center px-4 py-6">
+      <h2 className="capitalize mb-4 font-semibold text-xl">
+        Read my blog
+      </h2>
+
+      <div className="flex flex-col gap-6 w-full">
+        {cardsContent.map((card, index) => (
+          <Card key={index} className="rounded-none border-none shadow-none">
+            <CardContent className="flex justify-between items-start gap-4">
+              <div className="text-left flex flex-col gap-1">
+                <h2 className="mb-1 font-semibold text-lg"><Balancer>{card.title}</Balancer></h2>
+                <p className="text-sm">{card.description}</p>
+                <p className="text-xs mt-2 text-black/80">{card.date}</p>
+              </div>
+              <Image src={card.image} width={100} height={100} className="w-[24vw] h-full object-cover" />
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
+
